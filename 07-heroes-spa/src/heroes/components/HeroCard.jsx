9b25337b6e75a814ab1ad773ef18783b8a117755
Alex Farias dos Santos <fariasdossantosalex@gmail.com>
2023-02-07 15:46:56 -0300

    acomodo del card

--- 07-heroes-spa/src/heroes/components/HeroCard.jsx
+++ 07-heroes-spa/src/heroes/components/HeroCard.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 
 export const HeroItem = ( {id, superhero, publisher, alter_ego, first_appearance, characters} ) => {
 
@@ -6,14 +7,24 @@ export const HeroItem = ( {id, superhero, publisher, alter_ego, first_appearance
   return (
    <div className="col">
       <div className="card bg-dark">
-          <div className="row no-gutters">
-            <div className="col-4">
+          <div className="row no-gutters p-3">
+            <div className="col-4   ">
               <img src={ heroImageUrl } alt={ superhero } className="card-img" />
             </div>
             <div className="col-8">
-                <div className="card-body">
+                <div className="">
                   <h5 className="card-title"> { superhero } </h5>
                   <p className="card-text"> { alter_ego } </p>
+                  {
+                    (alter_ego !== characters) && (<p className="card-text"> { characters } </p>)
+                  }
+                  <p className="card-text">
+                      <small className="text-md-center"> { first_appearance } </small>
+                  </p>
+
+                  <Link to={`/hero/${id}`}>
+                    <button className="btn btn-dark link-info"> Ver más...</button>
+                  </Link>
                 </div>
             </div>
           </div>
