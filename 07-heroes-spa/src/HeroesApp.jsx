08837387f1ee2100324414e0bae76a0004fa0ba0
Alex Farias dos Santos <fariasdossantosalex@gmail.com>
2023-02-07 11:23:38 -0300

    cracion del nav y rutas

--- 07-heroes-spa/src/HeroesApp.jsx
+++ 07-heroes-spa/src/HeroesApp.jsx
@@ -1,8 +1,9 @@
+import { AppRouter } from "./router/AppRouter"
 
 export const HeroesApp = () => {
   return (
         <>
-            <h1>Horeoes App</h1>
+            <AppRouter />
         </>  
    )
 }
---
+++ 07-heroes-spa/src/ui/components/NavBar.jsx
@@ -0,0 +1,56 @@
+import { Link, NavLink } from "react-router-dom"
+
+export const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand bg-dark rounded">
+        <div className="container-fluid">
+            <div className="collapse navbar-collapse"  id="navbarNav">
+                <ul className="navbar-nav">
+
+                    <Link
+                        className="navbar-brand" 
+                        to="/"
+                    >
+                        Asociaciones
+                    </Link>    
+
+                    <li className="nav-item">
+                        <NavLink
+                            className={ ({ isActive }) => `nav-link ${ isActive ? 'active' : '' }`}
+                            to="marvel"
+                        >
+                            Marvel
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            className={ ({ isActive }) => `nav-link ${ isActive ? 'active' : '' }`}
+                            to="/dc"
+                        >
+                            DC
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink
+                            className={ ({ isActive }) => `nav-link ${ isActive ? 'active' : '' }`}
+                            to="/Search"
+                        >
+                            Search
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <ul className="navbar-nav ml-auto">
+                <span className="nav-item nav-link text-info text-nowrap">
+                    Alex Farias dos Santos
+                </span>
+                <button className="nav-item nav-link btn">
+                    Logout
+                </button>
+            </ul>
+        </div>
+    </nav>
+  )
+}
