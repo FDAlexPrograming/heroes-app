0ec688e8baec0fe74653fecd3282268c840d6a48
Alex Farias dos Santos <fariasdossantosalex@gmail.com>
2023-02-22 09:25:27 -0300

    creacion de las cards y efectos en en la misma, ruteo, useMemo.

--- 07-heroes-spa/src/heroes/components/HeroList.jsx
+++ 07-heroes-spa/src/heroes/components/HeroList.jsx
@@ -1,11 +1,11 @@
-import { heroes } from "../data/heroes"
+import { useMemo } from "react";
 import { getHeroesByPublisher } from "../helpers"
 import { HeroItem } from "./HeroCard";
 
 
 export const HeroList = ({ publisher}) => {
 
-    const heroes = getHeroesByPublisher( publisher );
+    const heroes = useMemo( ()=> getHeroesByPublisher( publisher ), [ publisher ] );  // eseMemo memoriza valores, useCallback memoriza funciones 
 
   return (
         <div className="row rows-cols-1 row-cols-md-3 g-3">
--- 07-heroes-spa/src/heroes/pages/HeroPage.jsx
+++ 07-heroes-spa/src/heroes/pages/HeroPage.jsx
@@ -1,6 +1,49 @@
+import React, { useMemo } from "react";
+import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { getHeroById } from "../helpers";
 
 export const HeroPage = () => {
+  const { id } = useParams();
+  const hero = useMemo( ()=> getHeroById( id ), [ id ] ); 
+  const navigate = useNavigate();
+
+  const onBack = () =>{
+    navigate(-1)
+  }
+
+  if (!hero) {
+    return <Navigate to="/marvel" />;
+  }
+
+ 
   return (
-    <h1>Hero</h1>
-  )
-}
+   <div className="row mt-5">
+    <div className="col-4">
+      <img 
+        src={ `/assets/heroes/${id}.jpg` }
+        alt={ hero.superhero }
+        className="img-thumbnail hero-image"
+        />
+    </div>
+
+    <div className="col-8">
+      <h3 className="hero-name"> { hero.superhero } </h3>
+      <ul className="list-group hero-info">
+        <li className="list-group-item"> <b> Alter ego:  </b> { hero.alter_ego } </li>
+        <li className="list-group-item"> <b> Estudio:  </b> { hero.publisher } </li>
+        <li className="list-group-item"> <b> Primera aparición:  </b> { hero.first_appearance } </li>
+      </ul>
+      <div className="hero-characters">
+
+      <h5 className="mt-3 "> Characters </h5>
+      <p> {hero.characters} </p>
+      <button 
+        className="btn btn-dark"
+        onClick={ onBack }
+        > Volver</button>
+    </div>
+      </div>
+   </div>
+  );
+};
+
