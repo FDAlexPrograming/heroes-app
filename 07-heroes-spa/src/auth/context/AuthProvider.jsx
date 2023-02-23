204e0961681c9efe7641473c979c2da836e4b5ef
Alex Farias dos Santos <fariasdossantosalex@gmail.com>
2023-02-23 08:53:15 -0300

    guardar el usuario en el localStorage

--- 07-heroes-spa/src/auth/context/AuthProvider.jsx
+++ 07-heroes-spa/src/auth/context/AuthProvider.jsx
@@ -3,22 +3,25 @@ import { types } from "../types/types"
 import { AuthContext } from "./AuthContext"
 import { authReducer } from "./authReducer"
 
-const initialState = {
-    logged: false,
+const init = () => {
+    const user = JSON.parse( localStorage.getItem('user') );
+    return {
+        logged: !!user, // doble negación -- si el usuario existe va a estar en true
+        user: user,
+    }
 }
 
 export const AuthProvider = ({ children }) => {
 
-   const [ state, dispatch ] = useReducer( authReducer, initialState );
+   const [ state, dispatch ] = useReducer( authReducer, {}, init );
 
    const login = async( name = '' ) => {
+    const user = { id: 'ABC', name }
     const action = {
         type:types.login,
-        payload: {
-            id: 'ABC',
-            name: name
-        }
+        payload: user
     }
+    localStorage.setItem('user', JSON.stringify( user ) );
     dispatch( action) ;
    }
 
